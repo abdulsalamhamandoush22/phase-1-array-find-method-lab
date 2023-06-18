3a06faec15d27d481dcf20775024e62b53e019fb
abdulsalamhamandoush <abdulsalamhamandoush22@gmail.com>
2023-06-18 04:29:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const winObject = record.find(entry => entry.result === 'W');
+    if (winObject) {
+      return winObject.year;
+    } else {
+      return undefined;
+    }
+  }
+  
